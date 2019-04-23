03ed8b2968009f3803a55eb33892814b052bdbea
Simran Kaur <32463659+kaursimran0019@users.noreply.github.com>
2019-04-23 21:35:52 +0530

    Add files via upload

---
+++ TestArrayList.java
@@ -0,0 +1,16 @@
+package com.test.collections;
+
+/*
+ * 1. Resizable-array implementation of the <tt>List</tt> interface.
+ * 2. Roughly Equivalent to Vector, except that it is unsynchronized.
+ * 3. Permits all the elements including null.
+ * 4. when Adding n elements we call the ensureCapacity Method, This prevents the incremental reallocation 
+ * 
+ */
+public class TestArrayList {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
